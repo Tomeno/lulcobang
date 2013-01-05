242b721f05beb32949f8905b75d4c9fb328e90af
michal.lulco@gmail.com <michal.lulco@gmail.com@272ff16f-aed8-189b-6d5f-730326a55c35>
2013-01-05 20:30:39 +0000

    pridana akcia general store

--- trunk/classes/commands/ChooseCardsCommand.php
+++ trunk/classes/commands/ChooseCardsCommand.php
@@ -10,8 +10,9 @@ class ChooseCardsCommand extends Command {
 
 	const NOT_POSSIBLE_CARD_CHOOSED = 4;
 
+	protected $template = 'cards-choice.tpl';
+	
 	protected function check() {
-
 		$possibleChoices = unserialize($this->actualPlayer['possible_choices']);
 		$possibleCards = $possibleChoices['drawn_cards'];
 		$possiblePickCount = $possibleChoices['possible_pick_count'];
@@ -46,6 +47,7 @@ class ChooseCardsCommand extends Command {
 			$handCards = unserialize($this->actualPlayer['hand_cards']);
 			$drawPile = unserialize($this->game['draw_pile']);
 			$throwPile = unserialize($this->game['throw_pile']);
+			$nextPlayerPossibleCards = array();
 			
 			foreach ($possibleCards as $possibleCard) {
 				if (in_array($possibleCard, $this->params)) {
@@ -53,11 +55,53 @@ class ChooseCardsCommand extends Command {
 				} else {
 					if ($restAction == 'back_to_deck') {
 						$drawPile[] = $possibleCard;
+					} elseif ($restAction == 'general_store') {
+						$nextPlayerPossibleCards[] = $possibleCard;
 					}
 				}
 			}
 
-			$this->actualPlayer['phase'] = Player::PHASE_PLAY;
+			if ($this->interTurnReason['action'] == 'general_store') {
+				$nextPositionPlayer = GameUtils::getPlayerOnNextPosition($this->game, $this->actualPlayer);
+				if ($nextPositionPlayer['id'] == $this->attackingPlayer['id']) {
+					$this->game['inter_turn_reason'] = '';
+					$this->game['inter_turn'] = 0;
+					
+					$this->attackingPlayer['phase'] = Player::PHASE_PLAY;
+					$this->attackingPlayer->save();
+				} else {
+					if ($this->actualPlayer['id'] == $this->attackingPlayer['id']) {
+						$this->actualPlayer['phase'] = Player::PHASE_WAITING;
+					} else {
+						$this->actualPlayer['phase'] = Player::PHASE_NONE;
+					}
+					$this->game['inter_turn'] = $nextPositionPlayer['id'];
+					
+					$cardRepository = new CardRepository();
+					$possibleCards = array();
+					foreach ($nextPlayerPossibleCards as $cardId) {
+						$possibleCards[] = $cardRepository->getOneById($cardId);
+					}
+
+					MySmarty::assign('possiblePickCount', 1);
+					MySmarty::assign('possibleCards', $possibleCards);
+					MySmarty::assign('possibleCardsCount', count($possibleCards));
+					MySmarty::assign('game', $this->game);
+					$response = MySmarty::fetch($this->template);
+
+					$playerPossibleChoices = array(
+						'drawn_cards' => $nextPlayerPossibleCards,
+						'possible_pick_count' => 1,
+						'rest_action' => 'general_store',
+					);
+					$nextPositionPlayer['phase'] = Player::PHASE_UNDER_ATTACK;
+					$nextPositionPlayer['possible_choices'] = serialize($playerPossibleChoices);
+					$nextPositionPlayer['command_response'] = $response;
+					$nextPositionPlayer->save();
+				}
+			} else {
+				$this->actualPlayer['phase'] = Player::PHASE_PLAY;
+			}
 			$this->actualPlayer['hand_cards'] = serialize($handCards);
 			$this->actualPlayer['command_response'] = '';
 			$this->actualPlayer['possible_choices'] = '';
